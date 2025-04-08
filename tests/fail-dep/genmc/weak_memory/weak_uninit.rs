0a17251f4bcdc4423eef0dc4f4bc054bb66df6ed
Patrick-6 <pamu99@gmx.ch>
2025-04-08 11:12:54 +0200

    Improve weak_uninit test

--- tests/fail-dep/genmc/weak_memory/weak_uninit.rs
+++ tests/fail-dep/genmc/weak_memory/weak_uninit.rs
@@ -1,4 +1,4 @@
-//@compile-flags: -Zmiri-ignore-leaks -Zmiri-genmc
+//@compile-flags: -Zmiri-genmc
 
 // Tests showing weak memory behaviours are exhibited. All tests
 // return true when the desired behaviour is seen.
@@ -13,9 +13,8 @@
 mod utils_dep;
 
 use std::ffi::c_void;
+use std::mem::MaybeUninit;
 use std::sync::atomic::*;
-
-use libc::{self, pthread_attr_t, pthread_t};
 
 use crate::utils_dep::*;
 
@@ -26,54 +25,31 @@ struct EvilSend<T>(pub T);
 unsafe impl<T> Send for EvilSend<T> {}
 unsafe impl<T> Sync for EvilSend<T> {}
 
-// We can't create static items because we need to run each test multiple times.
-fn static_uninit_atomic() -> &'static AtomicUsize {
-    unsafe { Box::leak(Box::new_uninit()).assume_init_ref() }
-}
+static mut F: MaybeUninit<usize> = MaybeUninit::uninit();
 
-extern "C" fn thread_1(value: *mut c_void) -> *mut c_void {
+extern "C" fn thread_1(_value: *mut c_void) -> *mut c_void {
     unsafe {
-        let x_ptr_ptr: *const *const AtomicUsize = std::mem::transmute(value);
-        let x: &AtomicUsize = &**x_ptr_ptr;
+        let x = AtomicUsize::from_ptr(&raw mut F as *mut usize);
         x.store(1, Ordering::Relaxed);
         std::ptr::null_mut()
     }
 }
 
-extern "C" fn thread_2(value: *mut c_void) -> *mut c_void {
+extern "C" fn thread_2(_value: *mut c_void) -> *mut c_void {
     unsafe {
-        let x_ptr_ptr: *const *const AtomicUsize = std::mem::transmute(value);
-        let x: &AtomicUsize = &**x_ptr_ptr;
-        x.load(Ordering::Relaxed); //~ERROR: using uninitialized data
+        let x = AtomicUsize::from_ptr(&raw mut F as *mut usize);
+        x.load(Ordering::Relaxed); //~ERROR: access violation, data race or IPR validity broken
         std::ptr::null_mut()
     }
-}
-
-fn relaxed() {
-    let x: &'static AtomicUsize = static_uninit_atomic();
-    let mut x_ptr: *const AtomicUsize = core::ptr::from_ref(x);
-    let param: *mut c_void = unsafe { std::mem::transmute(&raw mut x_ptr) };
-
-    let mut ids: [pthread_t; 2] = [0, 0];
-
-    let attr: *const pthread_attr_t = std::ptr::null();
-
-    if 0 != unsafe { libc::pthread_create(&raw mut ids[1], attr, thread_1, param) } {
-        std::process::abort();
-    }
-    if 0 != unsafe { libc::pthread_create(&raw mut ids[0], attr, thread_2, param) } {
-        std::process::abort();
-    }
-
-    unsafe { join_pthreads(ids) };
 }
 
 #[unsafe(no_mangle)]
 fn miri_start(_argc: isize, _argv: *const *const u8) -> isize {
     // Unlike with the non-GenMC version of this test, we should only need 1 iteration to detect the bug:
-    // for _ in 0..100 {
-    relaxed();
-    // }
+    unsafe {
+        let ids = create_pthreads_no_params([thread_1, thread_2]);
+        join_pthreads(ids);
+    }
 
     0
 }
